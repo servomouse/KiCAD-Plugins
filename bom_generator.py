89247713591af52cea9ccd92aa62ccc4e3eb183d
master <asd@mail.ru>
2022-07-05 21:59:07 +0300

    Windows popup added

--- bom_generator.py
+++ bom_generator.py
@@ -1,28 +1,45 @@
 import sys
 import os
 import csv
+import sys
 import subprocess
 import platform
+import webbrowser
 from platform import python_version
+
+
+def send_notification(message):
+    if platform.system() == "Windows":
+        subprocess.call(f"C:\\Windows\\system32\\msg.exe * {message}",shell=True)
+    elif platform.system() == "Darwin":
+        pass    # subprocess.Popen(["open", folder])
+    else:
+        os.system(f'notify-send {message}')
+
 try:
     import kicad_netlist_reader
+    import pentagon_hacker
 except ImportError:
-    os.system('notify-send "kicad_netlist_reader is not installed!"')
+    send_notification("kicad_netlist_reader is not installed!")
     print("Please run pip install kicad_netlist_reader")
     sys.exit()
 
 
-def open_file(folder, file):
+def open_file(folder, full_path):
     if platform.system() == "Windows":
-        os.startfile(folder)
+        # print(f'{folder}/{file}')
+        path = full_path.replace("/", "\\")
+        os.system(f'%windir%\\explorer /select, {path}')
+        # os.startfile(f'{full_path}.csv', 'explore')
     elif platform.system() == "Darwin":
         subprocess.Popen(["open", folder])
     else:
-        # subprocess.Popen(["xdg-open", '/select', file])
-        subprocess.call(["nautilus", f"{folder}/{file}"])
+        subprocess.call(["nautilus", f"{full_path}"])
 
 
 def main(input_file, output_file):
+    if output_file.find(".csv") == -1:
+        output_file += ".csv"
     if python_version()[0] != '3':
         print("You are trying to run this script with Python 2! Run it with python 3")
         sys.exit()
@@ -53,7 +70,7 @@ def main(input_file, output_file):
     f.close()
     filename = output_file.split("/")[-1]
     folder_path = output_file.replace(f"/{filename}", '')
-    open_file(folder_path, filename)
+    open_file(folder_path, output_file)
 
 
 if __name__ == '__main__':
